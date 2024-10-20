0adf36e21d9d61e2f5570d5afb04ef4b918d7b09
Le Hoang Than <lehoangthan584@gmail.com>
2024-10-20 22:07:26 +0700

    10202024

--- Than_DH52201426/Than_DH52201426/BangBam.h
+++ Than_DH52201426/Than_DH52201426/BangBam.h
@@ -264,7 +264,7 @@ namespace BangBam
 				{
 				case '1':	//Thêm khóa vào bảng băm
 				{
-					int the_key;
+					TYPEINFO the_key;
 					cout << "Nhap khoa can them vao: ";
 					cin >> the_key;
 					insert(hash_table, the_key);
@@ -497,11 +497,34 @@ namespace BangBam
 
 #pragma region Hiển thị bảng băm
 
+		//Hiển thị bảng băm hash_table
+		void display(HASHTABLE hash_table)
+		{
+			cout << "Bang bam: " << endl;
+			for (int i = 0; i < hash_table.m; i++)
+			{
+				cout << "Phan tu " << i << ": " << hash_table.array[i].key;
+				cout << " -> " << hash_table.array[i].next << endl;
+			}
+			cout << "So khoa hien tai: " << hash_table.count << endl;
+		}
+
 #pragma endregion
 
 
 
 #pragma region Hủy bỏ bảng băm
+
+		//Hủy bỏ bảng băm hash_table
+		void clear(HASHTABLE& hash_table)
+		{
+			for (int i = 0; i < hash_table.m; i++)
+			{
+				hash_table.array[i].key = FREE;		//Phần tử trống
+				hash_table.array[i].next = FREE;		//Không có phần tử kế tiếp
+			}
+			hash_table.count = 0;					//Số khóa hiện tại = 0
+		}
 
 #pragma endregion
 
@@ -511,6 +534,342 @@ namespace BangBam
 		//Menu chức năng cho Phương pháp kết nối hợp nhất
 		void menu()
 		{
+			int m = 11;
+			HASHTABLE hash_table;
+			init(hash_table, m);	//Tạo hash_table với m phần tử
+
+			char option = {};
+			do
+			{
+				system("cls");
+				cout << "Cac tac vu cua bang bam:" << endl;
+				cout << "1. Them khoa vao bang bam." << endl;
+				cout << "2. Tim kiem khoa trong bang bam." << endl;
+				cout << "3. Loai bo khoa trong bang bam." << endl;
+				cout << "4. Hien thi bang bam." << endl;
+				cout << "5. Huy bo bang bam." << endl;
+				cout << "0. Ket thuc." << endl;
+				cout << "Chon tac vu: ";
+				cin >> option;
+				switch (option)
+				{
+				case '1':	//Thêm khóa vào bảng băm
+				{
+					TYPEINFO the_key;
+					cout << "Nhap khoa can them vao: ";
+					cin >> the_key;
+					if (insert(hash_table, the_key) == false)
+					{
+						cout << "Bang bam bi day." << endl;
+						system("pause");
+					}
+					break;
+				}
+				case '2':	//Tìm khóa trong bảng băm
+				{
+					TYPEINFO the_key;
+					cout << "Nhap khoa can tim kiem: ";
+					cin >> the_key;
+					int pos = find(hash_table, the_key);
+					if (pos == FREE)
+						cout << "Khong tim thay khoa " << the_key << endl;
+					else
+						cout << "Tim thay khoa" << hash_table.array[pos].key << endl;
+					system("pause");
+					break;
+				}
+				case '3':	//Loại bỏ khóa trong bảng băm
+				{
+					TYPEINFO the_key;
+					cout << "Nhap khoa can loai bo: ";
+					cin >> the_key;
+					if (!remove(hash_table, the_key))
+					{
+						cout << "Khong co khoa " << the_key << endl;
+						system("pause");
+					}
+					break;
+				}
+				case '4':	//Hiển thị bảng băm
+				{
+					display(hash_table);
+					system("pause");
+					break;
+				}
+				case '5':	//Hủy bỏ bảng băm
+				{
+					clear(hash_table);
+					break;
+				}
+
+				default:
+					break;
+				}
+			} while (option != '0');
+		}
+#pragma endregion
+
+	}
+
+
+
+	namespace PhuongPhapThuTuyenTinh
+	{
+#pragma region Cấu trúc dữ liệu
+
+		const int FREE = -1;
+
+		typedef int TYPEINFO;
+		struct HASHTABLE		//Cấu trúc của bảng băm
+		{
+			TYPEINFO* array;	//Bảng băm
+			int count;			//Số khóa của hiện tại
+			int m;				//Số phần tử của bảng băm
+		};
+
+		//Hàm băm
+		int hashFunction(int m, TYPEINFO the_key)
+		{
+			return the_key % m;
+		}
+
+#pragma endregion
+
+
+
+#pragma region Khởi tạo bảng băm
+
+		//Tạo bảng băm hash_table trống có m phần tử
+		void init(HASHTABLE& hash_table, int m)
+		{
+			hash_table.array = new TYPEINFO[m];		//Tạo bảng băm có m phần tử
+			for (int i = 0; i < m; i++)
+				hash_table.array[i] = FREE;			//Phần tử [i] trống
+			hash_table.count = 0;					//Số khóa hiện tại = 0
+			hash_table.m = m;						//Số phần tử = m
+		}
+
+#pragma endregion
+
+
+
+#pragma region Thêm khóa vào bảng băm
+
+		//Thêm khóa the_key vào bảng băm hash_table
+		bool insert(HASHTABLE& hash_table, TYPEINFO the_key)
+		{
+			//Bảng băm bị đầy (còn 1 phần tử trống)
+			if (hash_table.count == hash_table.m - 1)
+				return false;					//Không thêm khóa vào được
+			else
+			{
+				//Địa chỉ băm của the_key
+				int i = hashFunction(hash_table.m, the_key);
+				//Tìm phần tử trống để thêm khóa the_key
+				//Phần tử [i] khác trống
+				while (hash_table.array[i] != FREE)
+				{
+					i++;						//i đến phần tử kế tiếp
+					//i quay vòng về đầu bảng băm (nếu cần)
+					i = i % hash_table.m;
+				}
+				hash_table.array[i] = the_key;	//Thêm th_key tại phần tử [i]
+				hash_table.count++;				//Tăng số khóa hiện tại
+			}
+		}
+
+#pragma endregion
+
+
+
+#pragma region Tìm kiếm khóa trong bảng băm
+
+		//Tìm khóa the_key trong bảng băm hash_table
+		int find(HASHTABLE hash_table, TYPEINFO the_key)
+		{
+			//Địa chỉ băm của the_key
+			int i = hashFunction(hash_table.m, the_key);
+			while (hash_table.array[i] != FREE)
+			{
+				//Khóa của phần tử [i] = the_key
+				if (hash_table.array[i] == the_key)
+					return i;	//Tìm thấy
+				else
+				{
+					i++;		//i đến phần tử kế tiếp
+					//i quay vòng về đầu hàng băm nếu cần
+					i = i % hash_table.m;
+				}
+			}
+			return -1;			//Không tìm thấy
+		}
+
+#pragma endregion
+
+
+
+#pragma region Loại bỏ khóa trong bảng băm
+
+		//Loại bỏ phần tử [i] trong bảng băm hash_table
+		void removePosition(HASHTABLE& hash_table, int i)
+		{
+			bool conti = true;				//Còn tiếp tục
+			bool inside = true;				//Còn tồn tại trong
+			do
+			{
+				hash_table.array[i] = FREE;	//Phần tử [i] trống
+				//Chép phần tử [i] khác trống kế tiếp vào phần tử [j] trống
+				int j = i;					//j là i
+				do
+				{
+					i++;					//i đến phần tử kế tiếp
+					//i quay vòng về đầu bảng băm (nếu cần)
+					i = i % hash_table.m;
+					if (hash_table.array[i] == FREE)	//Phần tử [i] trống
+						conti = false;					//Không tiếp tục
+					else								//Phần tử [i] khác trống
+					{
+						//Địa chỉ băm của khóa hash_table.array[i]
+						int r = hashFunction(hash_table.m, hash_table.array[i]);
+						//r ở bên trong vùng vòng từ chỉ số j đến chỉ số i
+						inside = ((j < r && r <= i) ||	//j <  r <= i
+							(r <= i && i < j) ||		//r <= i <  j
+							(i < j && j < r));			//i <  j <  r
+					}
+				} while (conti && inside);		//Còn tiếp tục, r còn bên trong
+				if (conti)						//Còn tiếp rục
+					//Chép phần tử [i] vào phần tử [j]
+					hash_table.array[j] = hash_table.array[i];
+			} while (conti);					//Còn tiếp tục
+		}
+
+		//Loại bỏ khóa the_key trong bảng băm hash_table
+		bool remove(HASHTABLE& hash_table, TYPEINFO the_key)
+		{
+			//Tìm khóa the_key trong bảng băm hash_table
+			int i = find(hash_table, the_key);
+			if (i == -1)			//Không tìm thấy
+				return false;
+			else
+			{
+				hash_table.count--;	//Giảm số khóa của bảng băm
+				//Loại bỏ phần tử [i] trong bảng băm hassh_table
+				removePosition(hash_table, i);
+				return true;
+			}
+		}
+
+#pragma endregion
+
+
+
+#pragma region Hiển thị bảng băm
+
+		//Hiển thị bảng băm hash_table
+		void display(HASHTABLE hash_table)
+		{
+			cout << "Bang bam: " << endl;
+			for (int i = 0; i < hash_table.m; i++)
+				//Hiển thị phần tử [i] của bảng băm 
+				cout << "Phan tu " << i << ": " << hash_table.array[i] << endl;
+			//Hiển thị số khóa của bảng băm
+			cout << "So phan tu: " << hash_table.count << endl;
+		}
+
+#pragma endregion
+
+
+
+#pragma region Hủy bỏ bảng băm
+
+		//Hủy bỏ bảng băm hash_table
+		void clear(HASHTABLE& hash_table)
+		{
+			for (int i = 0; i < hash_table.m; i++)
+				hash_table.array[i] = FREE;			//Phần tử i trống
+			hash_table.count = 0;					//Số khóa hiện tại = 0
+		}
+
+#pragma endregion
+
+
+
+#pragma region Menu
+		//Menu chức năng cho Phương pháp kết nối hợp nhất
+		void menu()
+		{
+			int m = 11;
+			HASHTABLE hash_table;
+			init(hash_table, m);	//Tạo hash_table với m phần tử
+
+			char option = {};
+			do
+			{
+				system("cls");
+				cout << "Cac tac vu cua bang bam:" << endl;
+				cout << "1. Them khoa vao bang bam." << endl;
+				cout << "2. Tim kiem khoa trong bang bam." << endl;
+				cout << "3. Loai bo khoa trong bang bam." << endl;
+				cout << "4. Hien thi bang bam." << endl;
+				cout << "5. Huy bo bang bam." << endl;
+				cout << "0. Ket thuc." << endl;
+				cout << "Chon tac vu: ";
+				cin >> option;
+				switch (option)
+				{
+				case '1':	//Thêm khóa vào bảng băm
+				{
+					TYPEINFO the_key;
+					cout << "Nhap khoa can them vao: ";
+					cin >> the_key;
+					if (insert(hash_table, the_key) == false)
+					{
+						cout << "Bang bam bi day." << endl;
+						system("pause");
+					}
+					break;
+				}
+				case '2':	//Tìm khóa trong bảng băm
+				{
+					TYPEINFO the_key;
+					cout << "Nhap khoa can tim kiem: ";
+					cin >> the_key;
+					int pos = find(hash_table, the_key);
+					if (pos == -1)
+						cout << "Khong tim thay khoa " << the_key << endl;
+					else
+						cout << "Tim thay khoa" << the_key << endl;
+					system("pause");
+					break;
+				}
+				case '3':	//Loại bỏ khóa trong bảng băm
+				{
+					TYPEINFO the_key;
+					cout << "Nhap khoa can loai bo: ";
+					cin >> the_key;
+					if (!remove(hash_table, the_key))
+					{
+						cout << "Khong co khoa " << the_key << endl;
+						system("pause");
+					}
+					break;
+				}
+				case '4':	//Hiển thị bảng băm
+				{
+					display(hash_table);
+					system("pause");
+					break;
+				}
+				case '5':	//Hủy bỏ bảng băm
+				{
+					clear(hash_table);
+					break;
+				}
+
+				default:
+					break;
+				}
+			} while (option != '0');
 		}
 #pragma endregion
 
@@ -533,7 +892,6 @@ namespace BangBam
 			cout << "  2. Phuong phap Ket noi Hop nhat." << endl;
 			cout << "\nB. Phuong phap Dia chi mo." << endl;
 			cout << "  3. Phuong phap thu tuyen tinh." << endl;
-			cout << "  4. Phuong phap thu bac 2." << endl;
 			cout << "\n0. Ket thuc." << endl;
 			cout << "Chon tac vu: ";
 			cin >> option;
@@ -543,6 +901,9 @@ namespace BangBam
 				PhuongPhapKetNoiTrucTiep::menu();
 				break;
 			case '2':
+				PhuongPhapKetNoiHopNhat::menu();
+				break;
+			case '3':PhuongPhapThuTuyenTinh::menu();
 				break;
 			default:
 				break;
